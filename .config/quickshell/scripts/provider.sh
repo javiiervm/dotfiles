2b5c2b4accc9d1804900023eaacd8e948c7cfb03
javiiervm <javiervillamart8@gmail.com>
2026-07-17 15:56:36 +0200

    Fixed wallpaper menu

--- .config/quickshell/scripts/provider.sh
+++ .config/quickshell/scripts/provider.sh
@@ -99,4 +99,11 @@ elif [ "$MODE" = "--bt" ]; then
     else
         echo "disabled||||state"
     fi
+
+elif [ "$MODE" = "--wallpaper" ]; then
+    WALL_DIR="$2"
+    [ ! -d "$WALL_DIR" ] && exit 1
+    ls -1 "$WALL_DIR" | while read -r wall; do
+        echo "${wall%.}|Apply wallpaper|$WALL_DIR/$wall|echo '$WALL_DIR/$wall' > /tmp/current_wallpaper; awww img \"$WALL_DIR/$wall\" --transition-type center --transition-step 60 --transition-fps 120 --transition-duration 2 && wal -i \"$WALL_DIR/$wall\" -n -q && cp \"$WALL_DIR/$wall\" ~/.cache/hyprlock/current_wallpaper.png && notify-send 'Theme synced' -i \"$WALL_DIR/$wall\"|cmd"
+    done
 fi
