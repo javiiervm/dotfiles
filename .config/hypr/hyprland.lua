6fe5f1dd5285a01cc323ba3132e9209b524f4436
javiiervm <javiervillamart8@gmail.com>
2026-08-26 11:39:40 +0200

    Bloq num variable

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -178,6 +178,33 @@ hl.config({
     },
 })
 
+-- hl.config({
+--     decoration = {
+--         rounding = 12,
+--         rounding_power = 2,
+
+--         active_opacity = 0.95,
+--         inactive_opacity = 0.91,
+
+--         shadow = {
+--             enabled = true,
+--             range = 6,
+--             render_power = 3,
+--             color = "rgba(1a1a1aaa)",
+--         },
+
+--         blur = {
+--             enabled = true,
+--             size = 8,
+--             passes = 2,
+--             new_optimizations = true,
+--             ignore_opacity = true,
+--             xray = false,
+--             vibrancy = 0.08,
+--         },
+--     },
+-- })
+
 -- https://wiki.hypr.land/Configuring/Variables/#animations
 
 hl.config({
@@ -282,6 +309,8 @@ hl.config({
         kb_model = "",
         kb_options = "",
         kb_rules = "",
+
+        numlock_by_default = true,
 
         follow_mouse = 1,
 
